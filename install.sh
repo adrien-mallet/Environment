f5ad6077a21d973eade6e09788d0cc37ad7094d3
adrien-mallet <adrien.mallet@gmail.com>
2023-07-11 22:10:21 +0200

    config: first commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+
+SCRIPT_DIR="${0:A:h}";
+
+parse_directory()
+{
+	if [ "$1" = "$SCRIPT_DIR/.git" ] || [ "$1" = "$SCRIPT_DIR/install.sh" ]
+	then
+		return;
+	fi
+	
+	for file in $1/*(D)
+	do
+		echo $file;
+		if [ -d "$file" ]; then 
+			mkdir ~${file#$SCRIPT_DIR}
+			parse_directory $file
+		else 
+			symlink $file
+		fi
+	done
+}
+
+symlink()
+{
+	ln -sf $1 ~${1#$SCRIPT_DIR};
+}
+
+parse_directory $SCRIPT_DIR
